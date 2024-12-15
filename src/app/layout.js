914e3bf6ccf71e6f115b53a34708d5cf410fb358
JeaminRhee <45053452+JeaminRhee@users.noreply.github.com>
2024-12-15 23:54:48 +0900

    added Images (Japan's Examples)

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ThemeProvider from './ThemeProvider';
+import ThemeProvider from './ThemeProvider.js';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="kr">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -73,19 +73,99 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <h1 className={styles.animatedText}>{displayText}</h1>
-        <h6 className={styles.redText}>불법주차 척결 & 생명 구하자</h6>
+        <h6 className={styles.redText}>불법주차 척결 & 인구 지키자</h6>
         <h6 className={styles.redText}><i>Eliminate illegal parking & save lives ✞</i></h6>
         <h2>1년 평균 교통사고 수: <br /><code><i>{count.toLocaleString()}</i></code> 건</h2>
         <h2>1년 평균 교통사고 사망자 수: <br /><code><i>{fatalities.toLocaleString()}</i></code> 건</h2>
         <h2>청소년 사망원인 2위: <br /><code>교통사고 및 안전사고</code></h2>
-        
+        <div className={styles.responsiveImageContainer}>
+          <Image
+            className={styles.responsiveImage}
+            src="/illegal_parkings01.PNG"
+            alt="부산불법주정차사진전01"
+            width={100}
+            height={45}
+          />
+        </div>
+        <a><code>대한민국 주차 문화 현실 ...</code> </a>
+        <div className={styles.responsiveImageContainer}>
+          <Image
+            className={styles.responsiveImage}
+            src="/illegal_parkings02.PNG"
+            alt="부산불법주정차사진전02"
+            width={100}
+            height={45}
+          />
+        </div>
+        <a><code>주차질서 의식 부족 ... <br/>근접 주차 선호 ...<br/>주차는 원래 공짜 인식 ...</code><br/><br/> 근접 주차를 선호하는 경향과 주차를 공짜로 이용하는 인식이 불법주정차 문제를 악화시킵니다.
+<br/><br/>
+이렇게 불법주정차 차량 때문에 목숨을 잃는 사람이 매년 <code>1,000명</code> 이상... <br/><br/></a>
+<a><code>반면 옆나라는 ...</code> </a>
+        <div className={styles.responsiveImageContainer}>
+          <Image
+            className={styles.responsiveImage}
+            src="/japan_example01.PNG"
+            alt="일본불법주정차없는이유01"
+            width={100}
+            height={45}
+          />
+        </div>
+        <div className={styles.responsiveImageContainer}>
+          <Image
+            className={styles.responsiveImage}
+            src="/japan_example02.PNG"
+            alt="일본불법주정차없는이유02"
+            width={100}
+            height={45}
+          />
+        </div>
+        <div className={styles.responsiveImageContainer}>
+          <Image
+            className={styles.responsiveImage}
+            src="/japan_example03.PNG"
+            alt="일본불법주정차없는이유03"
+            width={100}
+            height={45}
+          />
+        </div>
+        <div className={styles.responsiveImageContainer}>
+          <Image
+            className={styles.responsiveImage}
+            src="/japan_example04.PNG"
+            alt="일본불법주정차없는이유04"
+            width={100}
+            height={45}
+          />
+        </div>
+        <div className={styles.responsiveImageContainer}>
+          <Image
+            className={styles.responsiveImage}
+            src="/japan_example05.PNG"
+            alt="일본불법주정차없는이유05"
+            width={100}
+            height={45}
+          />
+        </div>
+        <div className={styles.responsiveImageContainer}>
+          <Image
+            className={styles.responsiveImage}
+            src="/japan_example06.PNG"
+            alt="일본불법주정차없는이유06"
+            width={100}
+            height={45}
+          />
+        </div>
         <div className={styles.ctas}>
+          <a className={styles.primary} href="/" rel="noopener noreferrer">
+            <Image className={styles.logo} src="/vercel.svg" alt="Vercel logomark" width={20} height={20} />
+            불법주정차 신고하고 기프티콘 받기
+          </a>
           <a className={styles.primary} href="/lectures" rel="noopener noreferrer">
             <Image className={styles.logo} src="/vercel.svg" alt="Vercel logomark" width={20} height={20} />
-            불법주정차 교육자료
+            불법주정차 신고방법 교육자료
           </a>
-          <a href="https://nextjs.org/docs" target="_blank" rel="noopener noreferrer" className={styles.secondary}>
-            우리의 docs 읽기
+          <a href="/" rel="noopener noreferrer" className={styles.secondary}>
+            Home
           </a>
         </div>
       </main>
